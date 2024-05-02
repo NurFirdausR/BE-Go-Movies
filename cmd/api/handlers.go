a0839110d5cfc10c13f6259650fc8630f3fdcaf6
NurFirdausR <nurfirdaus.5000@gmail.com>
2024-05-02 23:42:04 +0700

    feat: Helper Error json

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
-
-// type Payload struct {
-// 	Status  string `json:"status"`
-// 	Message string `json:"message"`
-// 	Version string `json:"version"`
-// }
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	var payload = struct {
@@ -23,29 +15,17 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
+	_ = app.writeJSON(w, http.StatusOK, payload)
 
-	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 
 	movies, err := app.DB.AllMovies()
 	if err != nil {
-		fmt.Println(err)
+		app.errorJSON(w, err)
+		return
 	}
-	out, err := json.Marshal(movies)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, movies)
 
 }
